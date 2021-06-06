3982dfc3ed6cb939b7acb365ab8e786419adb01c
Robson Candido dos Santos Alves <robsondesenvolvimento@gmail.com>
2021-06-05 23:03:44 -0300

    Include treatement error

--- js/scripts.js
+++ js/scripts.js
@@ -9,25 +9,41 @@
 async function people() {
 
     await fetch('https://robsonalves-srvapp.azurewebsites.net/api/peoples')
+        .then(response => {
+            if (!response.ok)
+                throw new Error(`Status: ${response.status}, StatusText: ${response.statusText}`);                
+            return response;
+        })
         .then(response => response.json())
         .then(people => {
             document.getElementById('nickname').innerHTML = people.nickname;
             document.getElementById('descriptionEmail').innerHTML = people.email;
+        })
+        .catch(error => {
+            console.log(`${error.message}`);
         });
-
 }
 
 people();
 
+
 async function socialMedias() {
 
     await fetch('https://robsonalves-srvapp.azurewebsites.net/api/socialmedia')
+        .then(response => {
+            if (!response.ok)
+                throw new Error(`Status: ${response.status}, StatusText: ${response.statusText}`);                
+            return response;
+        })
         .then(response => response.json())
         .then(socialMedia => {
             document.getElementById('linkedin').href = `${socialMedia.linkedIn}`;
             document.getElementById('github').href = `${socialMedia.github}`;
             document.getElementById('discord').href = `${socialMedia.discord}`;
             document.getElementById('emailCall').href = `mailto:${socialMedia.email}?subject=Mail from RobsonDev`;
+        })
+        .catch(error => {
+            console.log(`${error.message}`);
         });
 
 }
